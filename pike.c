834938d5bad79f77a33e6778a8c642f170375694
Kyryl Melekhin <k.melekhin@gmail.com>
2026-09-05 02:22:33 -0100

    pike.c: misc

--- pike.c
+++ pike.c
@@ -119,8 +119,7 @@ enum
 };
 
 typedef struct rsub rsub;
-struct rsub
-{
+struct rsub {
 	int ref;
 	rsub *freesub;
 	const char *sub[];
@@ -131,12 +130,10 @@ typedef struct {
 	int capsz;	/* cap_stack ints in it, also alt_stack's offset */
 } rctx;
 
-typedef struct rthread rthread;
-struct rthread
-{
+typedef struct rthread {
 	int *pc;
 	rsub *sub;
-};
+} rthread;
 
 #define INSERT_CODE(at, num, pc) \
 if (code) \
@@ -564,8 +561,7 @@ static int reg_comp(rcode *prog, char *re, rcode *hdr, rctx *ctx, int flg)
 	return 0;
 }
 
-/* compile re in two passes: the first counts, the second emits the code
- * into the buffer it sized; both work on the one stack in ctx */
+/* compile re in two passes: the first counts, the second emits the code */
 static rcode *re_make(char *re, int *nsubc, int flg)
 {
 	rcode hdr, *prog;
